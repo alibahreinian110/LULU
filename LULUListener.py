c412fe81ae686e1900175eb5e42b42fe6a3d51cb
alibahreinian110 <44114504+alibahreinian110@users.noreply.github.com>
2020-03-26 15:39:50 +0430

    grammer edited

--- LULUListener.py
+++ LULUListener.py
@@ -8,6 +8,33 @@ else:
 # This class defines a complete listener for a parse tree produced by LULUParser.
 class LULUListener(ParseTreeListener):
 
+    # Enter a parse tree produced by LULUParser#ralation_op.
+    def enterRalation_op(self, ctx:LULUParser.Ralation_opContext):
+        pass
+
+    # Exit a parse tree produced by LULUParser#ralation_op.
+    def exitRalation_op(self, ctx:LULUParser.Ralation_opContext):
+        pass
+
+
+    # Enter a parse tree produced by LULUParser#bitwise_op.
+    def enterBitwise_op(self, ctx:LULUParser.Bitwise_opContext):
+        pass
+
+    # Exit a parse tree produced by LULUParser#bitwise_op.
+    def exitBitwise_op(self, ctx:LULUParser.Bitwise_opContext):
+        pass
+
+
+    # Enter a parse tree produced by LULUParser#logic_op.
+    def enterLogic_op(self, ctx:LULUParser.Logic_opContext):
+        pass
+
+    # Exit a parse tree produced by LULUParser#logic_op.
+    def exitLogic_op(self, ctx:LULUParser.Logic_opContext):
+        pass
+
+
     # Enter a parse tree produced by LULUParser#keyword.
     def enterKeyword(self, ctx:LULUParser.KeywordContext):
         pass
